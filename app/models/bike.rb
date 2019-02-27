76be871e61fcbbad3208d6e13d17b192b9cc0a48
Martin Auvray <martinauvray@gmail.com>
2019-02-27 11:39:40 +0100

    1ere partie

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,11 @@
 class Bike < ApplicationRecord
   belongs_to :profil
+  validates :marque, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
+
 end
+
+validates :photo, presence: true
--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,3 +1,7 @@
 class Profil < ApplicationRecord
   belongs_to :user
+  validates :first_name, presence: true
+  validates :last_name, presence: true, uniqueness: { scope: :last_name }
+  validates :address, presence: true
+
 end
