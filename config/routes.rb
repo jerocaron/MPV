72b12e84b418b59a9100397ae15679136b763dac
Valentin Demandat <valentin_mg08@hotmail.fr>
2019-02-27 17:29:20 +0100

    profil show and its backend done, profil show to continue tomorrow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :bikes, only: [ :index, :create, :new, :show ]
-  resources :profils, only: [ :create, :new, :edit, :update ]
+  resources :profils, only: [ :create, :new, :edit, :update, :show ]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_140706) do
+ActiveRecord::Schema.define(version: 2019_02_27_154016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2019_02_26_140706) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tel"
+    t.string "iban"
+    t.string "photo"
     t.index ["user_id"], name: "index_profils_on_user_id"
   end
 
