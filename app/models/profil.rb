fc33343c613b074e45ee047218dca719ec086db8
Martin Auvray <martinauvray@gmail.com>
2019-02-27 12:02:04 +0100

    validation

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,4 +1,7 @@
 class Profil < ApplicationRecord
   belongs_to :user
   has_many :bikes
+  validates :first_name, presence: true
+  validates :last_name, presence: true, uniqueness: { scope: :last_name }
+  validates :address, presence: true
 end
