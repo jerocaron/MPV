2547ef52ffa167bb4ba51cf5f5755ef7bf1e2d93
Martin Auvray <martinauvray@gmail.com>
2019-02-28 17:47:37 +0100

    seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @bikes = Bike.all
+    if params[:address].present?
+      search
+    else
+      @bikes = Bike.all
+      @bike = Bike.new
+    end
 
     @markers = @bikes.map do |bike|
       {
@@ -10,4 +15,19 @@ class PagesController < ApplicationController
       }
     end
   end
+
+  private
+
+  def search
+     @bike = Bike.new
+     @bikes = Bike.where("address ILIKE ?", "%#{params[:address]}%")
+  #   @distance = Geocoder::Calculations.distance_between([@performance.profile.latitude, @performance.profile.longitude], [current_user.profile.latitude, current_user.profile.longitude])
+  end
 end
+
+
+
+# paris => 48.8529717 , 2.3477134,17
+# lyon => 45.757811,4.8317562,15.82
+# lille => 50.6286251,3.0474652,15.11
+
