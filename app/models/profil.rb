7bb792270013c091caf933d98c83891e3bdc1201
Valentin Demandat <valentin_mg08@hotmail.fr>
2019-03-01 16:41:20 +0100

    cards done

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -2,8 +2,8 @@ class Profil < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   
   belongs_to :user, optional: true
-  has_many :bikes
-  has_many :rents
+  has_many :bikes, dependent: :destroy
+  has_many :rents, dependent: :destroy
   # validates :first_name, presence: true
   # validates :last_name, presence: true, uniqueness: { scope: :last_name }
   # validates :address, presence: true
