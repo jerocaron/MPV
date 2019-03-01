0cf72b1978c4f2943bd56e82768df5dd44afd600
Martin Auvray <martinauvray@gmail.com>
2019-03-01 14:42:17 +0100

    finalisation homepage

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v9'
 
     });
 
